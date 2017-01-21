51e0227bfbd96fe5799bcec292633bcd832d0581
Tazim <Tazim@DESKTOP-AD0JRB3>
2017-01-21 13:22:11 +0600

    Some More bugged Code

--- Core/Converter/vBlockToCode.cs
+++ Core/Converter/vBlockToCode.cs
@@ -5,25 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using DTD.Entity;
 using DTD.Entity.Enum;
+using DTD.Entity.Helpers;
 using DTD.Entity.vCodes;
 
 namespace Core.Converter
 {
     public static class VBlockToCode
     {
-        public static void VCodeToCode(VCode vcode)
+        public static void VCodeToCode(VCode vcode,out string code)
         {
+          
             if (vcode.VType == Enums.VType.Function)
             {
-                Function function=(Function)vcode;
+                Function function=vcode;
+                code += function.AccessModifier;
+                code += function.Type;
+                code += function.Name;
+                code += "(";
+                code += ")";
+                ScopeToCode(function.Scope,out code);
             }
 
-
+            code = "";
         }
 
-        public static void ScopeToCode()
+        public static void ScopeToCode(Scope scope,out string code)
         {
-            
+            code += "{";
+            foreach (VCode item in scope.Items)
+            {
+                VCodeToCode(item,out code);
+            }
+            code += "}";
+
         }
 
         public static void ConditionToCode()
